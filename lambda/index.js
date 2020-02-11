f9006fa3a5eface3c3f74f47e7a7d858ef91cdd0
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-02-11 12:38:48 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -20,7 +20,7 @@ const languageStrings = {
         translation: {
             //WELCOME_MSG: 'आइए ',
             WELCOME_MSG: 'Vango! Eppidi irukkel?',
-            LOVE_MSG: '<amazon:emotion name="excited" intensity="medium">Naanum Unnai lovvu panren</amazon:emotion>',
+            LOVE_MSG: '<amazon:effect name="whispered">Naanum Unnai lovvu panren</amazon:emotion>',
             HELLO_MSG: 'Vanakkam',
             HELP_MSG: 'Eppidi udavi say atum ?',
             REFLECTOR_MSG: 'You just triggered {{intent}}',
